d1c6c617deb0b348dbd8baa6a746e0f1ec3d4eff
VINICIUS SANTOS OLIVEIRA <vo70346@gmail.com>
2022-03-14 08:43:52 -0300

    Estrutura de Decisão

--- Estrutura de Decisão/MaiorMenor.java
+++ Estrutura de Decisão/MaiorMenor.java
@@ -7,9 +7,6 @@ class MaiorMenor {
         System.out.print ("\nInforme sua idade: ");
         int idade = sc.nextInt();
 
-        // Delimitar um teste lógico para o limite inferior (1) e um teste lógico para o limite superior (120)
-        // Para limite inferior: >=1 // Para limite superior: <=120
-        // Temos dois testes lógicos: idade >=1 && idade <=120
         if (idade > 17) {
             System.out.print ("\n\tMaior de idade!\n");
         } else {
--- Estrutura de Decisão/MaiorMenor2.java
+++ Estrutura de Decisão/MaiorMenor2.java
@@ -7,6 +7,9 @@ class MaiorMenor2 {
         System.out.print("\nInforme sua idade: ");
         int idade = sc.nextInt();
 
+        // Delimitar um teste lógico para o limite inferior (1) e um teste lógico para o limite superior (120)
+        // Para limite inferior: >=1 // Para limite superior: <=120
+        // Temos dois testes lógicos: idade >=1 && idade <=120 (expressão lógica)
         if (idade >= 1 && idade <= 120) {
             if (idade >= 18) {
                 System.out.print("\n\tMaior de Idade!");
