87a84cfbbd6113aa9d9c0ead2e024e85c1a0fd4c
VINICIUS SANTOS OLIVEIRA <vo70346@gmail.com>
2022-03-16 10:53:03 -0300

    Exercicio 4

--- Listas de Exercícios/Lista de Exercícios 3 - Estrutura de Decisão/Exercicio4.java
+++ Listas de Exercícios/Lista de Exercícios 3 - Estrutura de Decisão/Exercicio4.java
@@ -7,14 +7,11 @@ class Exercicio4 {
         System.out.print("Informe um número inteiro: ");
         int n1 = sc.nextInt();
 
-        int lastNum = n1 % 2;
-
-        if (lastNum == 0) {
+        // é par se n%2==0
+        if (n1 % 2 == 0) {
             System.out.print("\n\t" + n1 + ": é um número par");
         } else {
             System.out.print("\n\t" + n1 + ": é um número ímpar");
         }
     }
 }
-
-// é par se n%2==0
