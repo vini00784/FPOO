d01c71bdd786bef6a1c19e543bc743c22fca49c6
VINICIUS SANTOS OLIVEIRA <vo70346@gmail.com>
2022-03-16 11:14:52 -0300

    Exercicio 8

--- Listas de Exercícios/Lista de Exercícios 3 - Estrutura de Decisão/Exercicio8.java
+++ Listas de Exercícios/Lista de Exercícios 3 - Estrutura de Decisão/Exercicio8.java
@@ -12,6 +12,26 @@ class Exercicio8 {
 
         double imc = peso / (altura * altura);
 
-        if ()
+        if (imc < 17) {
+            System.out.print("\n\tMuito abaixo do peso");
+        }
+        if (imc >= 17 && imc <= 18.49) {
+            System.out.print("\n\tAbaixo do peso");
+        }
+        if (imc >= 18.5 && imc <= 24.99) {
+            System.out.print("\n\tPeso normal");
+        }
+        if (imc >= 25 && imc <= 29.99) {
+            System.out.print("\n\tAcima do peso");
+        }
+        if (imc >= 30 && imc <= 34.99) {
+            System.out.print("\n\tObesidade I");
+        }
+        if (imc >= 35 && imc <= 39.99) {
+            System.out.print("\n\tObesidade II (severa)");
+        }
+        if (imc > 40) {
+            System.out.print("\n\tObesidade III (mórbida)");
+        }
     }
 }
