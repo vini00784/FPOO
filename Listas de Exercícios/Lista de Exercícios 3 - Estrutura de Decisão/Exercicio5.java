1f25e67163b1eb07292045d46c8c6fab4b62d5bd
VINICIUS SANTOS OLIVEIRA <vo70346@gmail.com>
2022-03-16 09:22:57 -0300

    Exercicio 5

--- Listas de Exercícios/Lista de Exercícios 3 - Estrutura de Decisão/Exercicio5.java
+++ Listas de Exercícios/Lista de Exercícios 3 - Estrutura de Decisão/Exercicio5.java
@@ -4,6 +4,13 @@ class Exercicio5 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("\nInforme sua idade: ");
+        int idade = sc.nextInt();
         
+        if (idade >= 18) {
+            System.out.print("\n\tMaior de Idade!");
+        } else {
+            System.out.print("\n\tMenor de Idade!");
+        }
     }
 }
