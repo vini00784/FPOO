131a77350932d19111d2eeea012c42062b5d82d4
VINICIUS SANTOS OLIVEIRA <vo70346@gmail.com>
2022-03-16 10:08:24 -0300

    Exercicio 8

---
+++ Listas de Exercícios/Lista de Exercícios 3 - Estrutura de Decisão/Exercicio8.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Exercicio8 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe seu peso: ");
+        double peso = sc.nextDouble();
+
+        System.out.print("Informe sua altura: ");
+        double altura = sc.nextDouble();
+
+        double imc = peso / (altura * altura);
+
+        if ()
+    }
+}
