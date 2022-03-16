4259de5eeef236b499c2ad3ff0a4baedb73ef9ad
VINICIUS SANTOS OLIVEIRA <vo70346@gmail.com>
2022-03-16 08:42:03 -0300

    Exercicio 5

---
+++ Listas de Exercícios/Lista de Exercícios 3 - Estrutura de Decisão/Exercicio4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Exercicio4 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe um número inteiro: ");
+        int n1 = sc.nextInt();
+    }
+}
