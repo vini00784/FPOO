bb707f8b58421fd1e4e42375b1fd40d5304de3bc
VINICIUS SANTOS OLIVEIRA <vo70346@gmail.com>
2022-03-23 11:06:33 -0300

    Testes

---
+++ Teste2.java
@@ -0,0 +1,11 @@
+class Teste2 {
+    public static void main(String args[]) {
+        System.out.println();
+        for (int i=20 ;i <= 50; ++i ) {
+            System.out.println(i);
+        }
+    }
+}
+
+// for (int i=0 ; i <=100; i++) {
+//             System.out.println("\n" + n + "X" + i + "=" + i);
