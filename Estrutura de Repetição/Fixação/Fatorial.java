52f00d064a0999e2f10025009b4783ae0558efe4
VINICIUS SANTOS OLIVEIRA <vo70346@gmail.com>
2022-03-28 11:22:21 -0300

    exerícios de fixação

--- Estrutura de Repetição/Fixação/Fatorial.java
+++ Estrutura de Repetição/Fixação/Fatorial.java
@@ -5,7 +5,7 @@ class Fatorial {
     public static void main(String args[]) {
         int n = 5;
         
-        for (int i = n ; i >= 1 ; --i) {
+        for (int i = 1 ; i <= 4 ; --i) {
             System.out.print("\n" + n + "! = " + ;
         }
     }
