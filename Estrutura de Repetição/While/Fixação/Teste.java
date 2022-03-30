0aa47b8460024935d8caae1953e8a7fb080f4365
VINICIUS SANTOS OLIVEIRA <vo70346@gmail.com>
2022-03-30 09:54:45 -0300

    Exercícios Finalizados

---
+++ Estrutura de Repetição/While/Fixação/Teste.java
@@ -0,0 +1,11 @@
+class Teste {
+    public static void main(String args[]) {
+        int i = 1;
+        
+        System.out.println();
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
