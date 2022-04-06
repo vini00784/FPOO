3bc4d8ddcaa6ddc0ed98ae6ba5e43dca71aac908
VINICIUS SANTOS OLIVEIRA <vo70346@gmail.com>
2022-04-06 08:25:38 -0300

    exercício 2 finalizado

--- Listas de Exercícios/Lista de Exercícios 4 - Estrutura de Repetição/Exercicio2.java
+++ Listas de Exercícios/Lista de Exercícios 4 - Estrutura de Repetição/Exercicio2.java
@@ -4,12 +4,27 @@ class Exercicio2 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
-        String usuarioCadastrado = "Jacinto Leite Aquino Rego";
-        String senha = "abc123";
+        String usuarioCadastrado = "Jacinto";
+        String senhaCadastrada = "abc123";
         int rep = 1;
 
         while (rep < 4) {
-            for ()
-        } 
+            for ( int i = 1; i <= 3; i++ ) {
+                System.out.println("Usuário: ");
+                String user = sc.next();
+
+                System.out.println("Senha: ");
+                String senha = sc.next();
+
+                if (user.equals(usuarioCadastrado) && senha.equals(senhaCadastrada)) {
+                    System.out.println("Seja bem-vindo(a)!");
+                    break;
+                } else {
+                    System.out.println("Usuário incorreto ou senha incorretos");
+                    rep++;
+                } 
+            }
+            System.exit(0);
+        }
     }
 }
