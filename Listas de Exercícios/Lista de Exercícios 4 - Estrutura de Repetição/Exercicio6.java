f516ea97b94c008e617e00c6770ee95e9e0eac35
VINICIUS SANTOS OLIVEIRA <vo70346@gmail.com>
2022-04-06 11:11:25 -0300

    exercício 6 finalizado

---
+++ Listas de Exercícios/Lista de Exercícios 4 - Estrutura de Repetição/Exercicio6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Exercicio6 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        String frase = "Desenvolvimento de Sistemas - Turma DS1-T";
+        char ch = '\u2588';
+
+        for (int i = 53 ; i>=1 ; i--) {
+            System.out.print(ch);
+        }
+        System.out.print( "\n" + ch + "     Desenvolvimento de Sistemas - Turma DS1-T     " + ch + "\n");
+        for (int i = 53 ; i>=1 ; i--) {
+            System.out.print(ch);
+        }
+    }
+}
