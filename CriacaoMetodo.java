71eea00afb1015c3794bfa9c692127e9a27164a5
VINICIUS SANTOS OLIVEIRA <vo70346@gmail.com>
2022-04-13 11:10:08 -0300

    exercício sobre criação de métodos aprimorado

--- CriacaoMetodo.java
+++ CriacaoMetodo.java
@@ -1,19 +1,22 @@
 // Implementar uma calculadora em JAVA que efetua as quatro operações algébricas, usando um método para cada alteração, sendo que cada um recebe e retorna números decimais.
+import javax.swing.JOptionPane;
 
 class CriacaoMetodo {
     public static void main(String args[]) {
-        double n1 = 7.5;
-        double n2 = 2.5;
+        JOptionPane jp = new JOptionPane();
+
+        double n1 = Double.parseDouble(jp.showInputDialog(null, "Informe um nmr qualquer: "));
+        double n2 = Double.parseDouble(jp.showInputDialog(null, "Informe outro nmr "));
 
         double ad = somar(n1, n2);
         double sub = subtrair(n1, n2);
         double mul = multiplicar(n1, n2);
         double div = dividir(n1, n2);
 
-        System.out.println("Soma = " + ad);
-        System.out.println("Subtração " + sub);
-        System.out.println("Multiplicação " + mul);
-        System.out.println("Divisão " + div);
+        jp.showMessageDialog(null, n1 + " + " + n2 + " eh igual a: " + ad);
+        jp.showMessageDialog(null, n1 + " - " + n2 + " eh igual a: " + sub);
+        jp.showMessageDialog(null, n1 + " * " + n2 + " eh igual a: " + mul);
+        jp.showMessageDialog(null, n1 + " / " + n2 + " eh igual a: " + div);
     }
 
     public static double somar(double n1, double n2) {
