cd04c54c9756a446d15b4ea21f66082ab63d2fc6
VINICIUS SANTOS OLIVEIRA <vo70346@gmail.com>
2022-04-13 08:01:06 -0300

    exercício 3 aprimorado

--- Listas de Exercícios/Lista de Exercícios 4 - Estrutura de Repetição/Exercicio3.java
+++ Listas de Exercícios/Lista de Exercícios 4 - Estrutura de Repetição/Exercicio3.java
@@ -3,21 +3,28 @@ import java.util.Scanner;
 class Exercicio3 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Insira a base: ");
-        long base = sc.nextLong();
+        long base = 0;
+        long exp = 0;
 
-        System.out.println("Insira o expoente");
-        long exp = sc.nextLong();
+
+        while (true) {
+            System.out.println("Insira a base: ");
+            base = sc.nextLong();
+            System.out.println("Insira o expoente");
+            exp = sc.nextLong();
+            if (base >= 0 && exp >= 0) {
+                break;
+            }
+        }
 
         long cont = base;
 
         for (long i = exp-1 ; i >=1 ; i--) {
             cont *= base;
+            if (exp==0) {
+                System.out.println(base + "^" + exp + " eh igual a: 1 ");
+        }
         }
         System.out.println(base + "^" + exp + " eh igual a: " + cont);
-
-        if (exp==0) {
-            System.out.println(base + "^" + exp + " eh igual a: 1 ");
-        }
     }
 }
